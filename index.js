70b4de3ddd5f17d72c1b724c5101d58fab1ff083
Zoran Culum <zoran.culum88@gmail.com>
2022-09-17 20:16:11 +0200

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+app.get('/health', (req, res) => {
+  res.send({
+    status: 'STABLE'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
